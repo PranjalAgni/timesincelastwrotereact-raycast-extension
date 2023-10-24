62715a88c88447fd37bd64a5101ab663b4b9e987
PranjalAgni <pranjal.agnihotri6@gmail.com>
2023-10-24 23:08:21 +0530

    :sunflower: listing my react projects with time since I last work on them

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import { List } from "@raycast/api";
+import { Detail, List } from "@raycast/api";
 import path from "path";
 import fs from "fs";
+import { format } from "timeago.js";
 import { IReactProjectMTime } from "./types";
 
 // Things I need to do:
@@ -17,29 +18,45 @@ const listReactProjectWithMtime = (basePath: string): Array<IReactProjectMTime> 
   const dirent = fs.readdirSync(basePath, { withFileTypes: true });
   const reactProjects = dirent
     .filter((dir) => dir.isDirectory())
-    .map((dir) => ({
-      name: dir.name,
-      mtime: fs.statSync(path.join(basePath, dir.name)).mtimeMs,
-    }));
+    .map((dir) => {
+      const dirStats = fs.statSync(path.join(basePath, dir.name));
+      return {
+        name: dir.name,
+        mtimems: dirStats.mtimeMs,
+        mtime: dirStats.mtime,
+      };
+    });
 
   return reactProjects;
 };
 
 const sortProjectsByLastModifiedTime = (projectA: IReactProjectMTime, projectB: IReactProjectMTime) => {
-  return projectB.mtime - projectA.mtime;
+  return projectB.mtimems - projectA.mtimems;
 };
 
+const computeTimeSpentSinceWrittenReact = (project: IReactProjectMTime) => {
+  const currentDate = new Date().getTime();
+  const projectDate = new Date(project.mtime).getTime();
+  return Math.round(Math.abs(currentDate - projectDate) / (1000 * 60 * 60 * 24)) + " days";
+  // return format(new Date(project.mtime));
+};
+//
 export default function Command() {
   console.log("Basename for dir: ", path.basename(__dirname));
   const projectsWithMetadata = listReactProjectWithMtime(REACT_BASE_DIR);
   projectsWithMetadata.sort(sortProjectsByLastModifiedTime);
   return (
     <List
+      isShowingDetail
       navigationTitle="React Projects"
       searchBarPlaceholder={`Search from your ${projectsWithMetadata.length} awesome react project`}
     >
       {projectsWithMetadata.map((project, idx) => (
-        <List.Item key={idx} title={project.name} />
+        <List.Item
+          key={idx}
+          title={project.name}
+          detail={<List.Item.Detail markdown={`**Last modified: ${computeTimeSpentSinceWrittenReact(project)}**`} />}
+        />
       ))}
     </List>
   );
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 export interface IReactProjectMTime {
   name: string;
-  mtime: number;
+  mtime: Date;
+  mtimems: number;
 }
