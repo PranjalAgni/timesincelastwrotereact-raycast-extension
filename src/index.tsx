c40af61db494f979392d81bac16aadbd8cc76979
PranjalAgni <pranjal.agnihotri6@gmail.com>
2024-01-27 23:05:36 +0530

    :green_heart: fixed the date and time issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { Detail, List } from "@raycast/api";
-import path from "path";
+import path, { dirname } from "path";
 import fs from "fs";
 import { format } from "timeago.js";
 import { IReactProjectMTime } from "./types";
@@ -15,27 +15,40 @@ import { IReactProjectMTime } from "./types";
 const REACT_BASE_DIR = "/Users/pranjal.dev/coding/React/";
 
 const isValidDir = (dir: fs.Dirent) => {
-  const blackListedDir = [".git", ".github", ".vscode", ".next", "node_modules", "public", "build", "dist", "coverage"];
-  return !blackListedDir.includes(dir.name);
+  const blackListedDir = ["node_modules", "public", "build", "dist", "coverage"];
+  return !(dir.name.length === 0 || dir.name.startsWith(".") || blackListedDir.includes(dir.name));
 };
 
-const getRecentUpdatedTime = (projectPath: string) => {
+const getRecentUpdatedTime = (
+  projectPath: string
+): {
+  mtimems: number;
+  mtime: Date;
+  dirname: string;
+} | null => {
   const projectDirectoriesQueue = [projectPath];
   const mostRecentUpdate = {
-    mtimems: Number.MAX_SAFE_INTEGER,
+    mtimems: 0,
     mtime: new Date(),
     dirname: "",
   };
+  let isFirstRun = true;
   while (projectDirectoriesQueue.length > 0) {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const dirname = projectDirectoriesQueue.shift()!;
     const dirent = fs.readdirSync(dirname, { withFileTypes: true });
-    dirent
-      .filter((dir) => dir.isDirectory() && isValidDir(dir))
+
+    const validDirent = dirent.filter((dir) => dir.isDirectory() && isValidDir(dir));
+    if (validDirent.length === 0 && isFirstRun) {
+      return null;
+    }
+
+    isFirstRun = false;
+    validDirent
       .map((dir) => path.join(dirname, dir.name))
       .forEach((dir) => {
         const dirStats = fs.statSync(dir);
-        if (Math.round(dirStats.mtimeMs) < Math.round(mostRecentUpdate.mtimems)) {
+        if (Math.round(dirStats.mtimeMs) > Math.round(mostRecentUpdate.mtimems)) {
           mostRecentUpdate.mtimems = dirStats.mtimeMs;
           mostRecentUpdate.mtime = dirStats.mtime;
           mostRecentUpdate.dirname = path.basename(dir);
@@ -50,9 +63,10 @@ const getRecentUpdatedTime = (projectPath: string) => {
 const listReactProjectWithMtime = (basePath: string): Array<IReactProjectMTime> => {
   const dirent = fs.readdirSync(basePath, { withFileTypes: true });
   const reactProjects = dirent
-    .filter((dir) => dir.isDirectory())
+    .filter((dir) => dir.isDirectory() && isValidDir(dir))
     .map((dir) => {
       const dirTimeStats = getRecentUpdatedTime(path.join(basePath, dir.name));
+      if (dirTimeStats === null) return null;
       return {
         name: dir.name,
         mtimems: dirTimeStats.mtimems,
@@ -61,24 +75,28 @@ const listReactProjectWithMtime = (basePath: string): Array<IReactProjectMTime> 
       };
     });
 
-  return reactProjects;
+  const filteredProjects = reactProjects.filter((project) => project !== null) as IReactProjectMTime[];
+  return filteredProjects;
 };
 
 const sortProjectsByLastModifiedTime = (projectA: IReactProjectMTime, projectB: IReactProjectMTime) => {
   return projectB.mtimems - projectA.mtimems;
 };
 
-const computeTimeSpentSinceWrittenReact = (project: IReactProjectMTime) => {
-  return format(project.mtime);
+const daysSinceWrittenReact = (project: IReactProjectMTime) => {
   const currentDate = new Date().getTime();
   const projectDate = new Date(project.mtime).getTime();
   return Math.round(Math.abs(currentDate - projectDate) / (1000 * 60 * 60 * 24)) + " days";
 };
 
+const localDateAndTimeOfProject = (project: IReactProjectMTime) => {
+  return new Date(project.mtime).toLocaleString("en-US", { timeZone: "Asia/Kolkata" });
+};
+
 export default function Command() {
   const projectsWithMetadata = listReactProjectWithMtime(REACT_BASE_DIR);
   projectsWithMetadata.sort(sortProjectsByLastModifiedTime);
-  console.log("All the projects: ", JSON.stringify(projectsWithMetadata, null, 2));
+  // console.log("All the projects: ", JSON.stringify(projectsWithMetadata, null, 2));
   return (
     <List
       isShowingDetail
@@ -89,7 +107,16 @@ export default function Command() {
         <List.Item
           key={idx}
           title={project.name}
-          detail={<List.Item.Detail markdown={`**Last modified: ${computeTimeSpentSinceWrittenReact(project)}**`} />}
+          detail={
+            <List.Item.Detail
+              markdown={`**Last modified: ${daysSinceWrittenReact(project)}**`}
+              metadata={
+                <List.Item.Detail.Metadata>
+                  <List.Item.Detail.Metadata.Label title="Date and Time" text={localDateAndTimeOfProject(project)} />
+                </List.Item.Detail.Metadata>
+              }
+            />
+          }
         />
       ))}
     </List>
